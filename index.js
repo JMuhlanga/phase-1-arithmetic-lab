c714e51f624dd641cffdf90200855b9369db1343
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-18 20:23:08 +0300

    Comnpleted on Aug 18

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+ let multiply;
+ let num1;
+ num1 = 31;
+ let num2;
+ num2 = 2;
+ multiply = num1 * num2;
+
+ let random =0;
+ random += 1;
+
+ let num3;
+ num3 =52;
+ let num4;
+ num4 = 12;
+ let mod;
+ mod = num3 % num4;
+
+ let max;
+ max = Math.max(5,10,15,20);
+  
+
+
